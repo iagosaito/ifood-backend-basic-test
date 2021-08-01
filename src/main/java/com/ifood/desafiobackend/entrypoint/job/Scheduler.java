d6bf8c14ab3c268690c50bbd516995801c76d6fd
iagosaito <iagosaito1999@gmail.com>
2021-07-31 22:57:02 -0300

    adding remove cache strategy

--- src/main/java/com/ifood/desafiobackend/entrypoint/job/Scheduler.java
+++ src/main/java/com/ifood/desafiobackend/entrypoint/job/Scheduler.java
@@ -1,17 +1,25 @@
 package com.ifood.desafiobackend.entrypoint.job;
 
+import com.ifood.desafiobackend.entrypoint.rest.CacheOpenWeatherApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.time.LocalDateTime;
+
 @EnableAsync
 @Configuration
 public class Scheduler {
 
+    @Autowired
+    private CacheOpenWeatherApi cacheOpenWeatherApi;
+
     @Async
     @Scheduled(fixedRate = 2000)
     public void cleanOutDateWeatherInCache() {
-        System.out.println("print...");
+        final LocalDateTime cleanCacheEventAt = LocalDateTime.now();
+        cacheOpenWeatherApi.cleanOutDatedCache(cleanCacheEventAt);
     }
 }
